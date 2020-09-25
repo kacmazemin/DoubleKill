baec64de92ca5a90ffcf8868e89e347101fc86f9
mehmetemin <kacmazemin72@gmail.com>
2020-09-26 01:06:17 +0300

    feat: converted BP code to Cpp to use viewBlend

--- Source/FPSGame/Private/FPSGameMode.cpp
+++ Source/FPSGame/Private/FPSGameMode.cpp
@@ -2,7 +2,7 @@
 
 #include "FPSGameMode.h"
 #include "FPSHUD.h"
-#include "FPSCharacter.h"
+#include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFPSGameMode::AFPSGameMode()
@@ -20,6 +20,30 @@ void AFPSGameMode::CompleteMission(APawn* InstigatorPawn)
 	if(InstigatorPawn)
 	{
 		InstigatorPawn->DisableInput(nullptr);
+
+		if(SpectatingViewPoint)
+		{
+			TArray<AActor*> ReturnedActors;
+			UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewPoint, ReturnedActors);
+
+			//Changing viewTarget. Check BP_GameMode for blueprint version of this block
+			if(ReturnedActors.Num() > 0)
+			{
+				AActor* NewViewTarget = ReturnedActors[0];
+
+				APlayerController* PlayerController = Cast<APlayerController>(InstigatorPawn->GetController());
+
+				if(PlayerController)
+				{
+					PlayerController->SetViewTargetWithBlend(NewViewTarget, .5f, EViewTargetBlendFunction::VTBlend_Cubic);
+				}
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("Please specify SpectatingViewPoint class from BP_GameMode"))
+		}
+
 	}
 
 	OnMissionCompleted(InstigatorPawn);
