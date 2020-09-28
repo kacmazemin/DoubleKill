543d9a9462a5bf3ae4cdeb7885cac359fdc1abb3
mehmetemin <kacmazemin72@gmail.com>
2020-09-28 21:04:36 +0300

    feat: created AIGuard class

---
+++ Source/FPSGame/Public/AIGuard.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+#include "AIGuard.generated.h"
+
+
+class UPawnSensingComponent;
+
+UCLASS()
+class FPSGAME_API AAIGuard : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAIGuard();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPawnSensingComponent* PawnSensingComponent;
+
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
